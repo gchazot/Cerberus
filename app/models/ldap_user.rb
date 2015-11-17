4ee3a723029daa6c16dfc2b4f46e1dae49ce5523
Maxime SIQUIER <maxime.siquier@amadeus.com>
2015-11-17 14:36:34 +0100

    Fix an issue where nil was sent instead of N/A suring all_info retrieval

--- app/models/ldap_user.rb
+++ app/models/ldap_user.rb
@@ -2,7 +2,7 @@ class LdapUser  < ActiveLdap::Base
 
  #LDAP_FILTERS = YAML.load_file('config/ldap_filters.yml').deep_symbolize_keys()
 
- ldap_mapping :dn_attribute => LDAP_FILTERS[:ldap_mapping][:dn_attribute], 
+ ldap_mapping :dn_attribute => LDAP_FILTERS[:ldap_mapping][:dn_attribute],
   :prefix => LDAP_FILTERS[:ldap_mapping][:prefix],
   :classes => LDAP_FILTERS[:ldap_mapping][:classes]
 
@@ -23,29 +23,29 @@ class LdapUser  < ActiveLdap::Base
     end
 
     return user_groups
- end  
+ end
 
  def self.belongs_to_group username, groupname
     user_groups = retrieve_groups username
     user_groups.include? groupname
- end    
- 
+ end
+
  def self.retrieve_all_information username
   user_info = Hash.new
   wished_fields = LDAP_FILTERS[:ldap_user_wished_fields]
   if exists? username
     ldap_user_info = find(username)
     wished_fields.each do |field, key|
-      if ldap_user_info.respond_to?(field)
+      if ldap_user_info.respond_to?(field) && ldap_user_info[field]!=nil
         user_info[key] = ldap_user_info[field]
       else
-        user_info[key] = "N/A" 
+        user_info[key] = "N/A"
       end
     end
     user_info.symbolize_keys!
   else
-      logger.error "Error during search user : #{username} in LDAP"  
-  end  
+      logger.error "Error during search user : #{username} in LDAP"
+  end
   return user_info
 end
 
